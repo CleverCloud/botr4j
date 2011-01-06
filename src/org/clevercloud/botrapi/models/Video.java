0b5a80b41b0cf01b6a677689f1edd870e1b6597d
blouerat <b.louerat@gmail.com>
2011-01-06 23:32:33 +0100

    class Video + gson 1.6

---
+++ src/org/clevercloud/botrapi/models/Video.java
@@ -0,0 +1,31 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.clevercloud.botrapi.models;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Set;
+
+/**
+ *
+ * @author bastien
+ */
+public class Video {
+
+    private String author;
+    private List<String> custom;
+    private Date date;
+    private String description;
+    private float duration;
+    private String key;
+    private String link;
+    private String mediatype;
+    private String status;
+    private Set<String> tags;
+    private String title;
+    private long views;
+
+}
--- src/testbotr/Main.java
+++ src/testbotr/Main.java
@@ -4,21 +4,10 @@
  */
 package testbotr;
 
-import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.net.URLEncoder;
-import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Properties;
-import java.util.Random;
 import org.clevercloud.botrapi.BotrAPI;
 
 /**
